9702c586597a10de9dc1fc09debaefd6d1254320
Prajul Chaudhary <prajchaudhary@deloitte.com>
2022-05-17 23:19:19 +0530

    added PairsPossible class

--- Mini Assignment 1/Assignment1.py
+++ Mini Assignment 1/Assignment1.py
@@ -11,7 +11,29 @@ class StringClass:
         chars = list(self.str)
         print(chars)
 
+class PairsPossible(StringClass):
+    string = ""
+    arr = []
+    def __init__(self):
+        self.string = input("Enter string for pairing : ")
+        pass
+
+    def storingPairs(self):
+        for i in range(len(self.string)):
+            for j in range(i + 1, len(self.string)):
+                self.arr.append(self.string[i] + self.string[j])
+
+    def printList(self):
+        for i in range(len(self.arr)):
+            print(self.arr[i] + " ", end='')
+        print()
+
+
 str = input("Please input string : ")
 obj = StringClass(str)
 print(obj.length_of_string())
 obj.list_of_character()
+
+obj1 = PairsPossible()
+obj1.storingPairs()
+obj1.printList()
