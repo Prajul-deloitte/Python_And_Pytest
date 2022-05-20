872f5cc8bbc5168d31e703643386d119f14becba
Prajul Chaudhary <prajchaudhary@deloitte.com>
2022-05-20 17:36:41 +0530

    Added problem 5 solution

--- Mini Assignment - 2/Assignment 2.py
+++ Mini Assignment - 2/Assignment 2.py
@@ -51,3 +51,13 @@ for i in range(len(keys)):
 
 print(dict)
 print()
+
+# Problem 5
+print("Problem 5 Solution")
+
+dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+
+dict1.update(dict2)
+print(dict1)
+print()
